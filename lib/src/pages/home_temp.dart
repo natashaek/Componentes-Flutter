31ccb3262c4c6c5e1afb6d26af214fd8e511d6e5
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-16 20:39:05 -0500

    Commit ListTiles

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,20 +10,25 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          )
-        ],
+        children: _crearItems(),
       ),
     );
+  }
+
+  List<Widget> _crearItems() {
+    // ignore: deprecated_member_use
+    List<Widget> lista = [];
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista
+        ..add(tempWidget)
+        ..add(Divider());
+    }
+
+    return lista;
   }
 }
