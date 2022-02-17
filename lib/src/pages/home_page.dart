e49bfda95e65d279a8604244a1130e96c9d0a599
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-17 13:24:26 -0500

    Commit Future Builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,20 +13,33 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data, context),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-    ];
+  List<Widget> _listaItems(data, context) {
+    final List<Widget> opciones = [];
+    if (data == null) {
+      return [];
+    }
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -13,7 +13,7 @@ class _MenuProvider {
     final resp = await rootBundle.loadString('data/menu_opts.json');
 
     Map dataMap = json.decode(resp);
-    print(dataMap['rutas']);
+    //print(dataMap['rutas']);
     opciones = dataMap['rutas'];
 
     return opciones;
