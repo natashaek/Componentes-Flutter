d338511519818373ef48cfe04b07f8a2a448b520
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-18 18:46:52 -0500

    Commit Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://elcomercio.pe/resizer/I0nAnN1Z6W2ernf6CRydrL356ZE=/1200x800/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/I7AKOS3WLNFO3IMDJBFHSAKMPY.jpg'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            child: CircleAvatar(
+              child: Text('BBH'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.allkpop.com/upload/2021/03/content/261108/web_data/allkpop_1616771628_untitled-1.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_location),
